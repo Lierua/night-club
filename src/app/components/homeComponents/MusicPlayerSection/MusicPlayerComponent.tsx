991949c2d0bf8bdf32d93c9c334cb7570441e350
Oliver <oliverbengtsson67@gmail.com>
2025-12-12 10:42:35 +0100

    ++++

--- src/app/components/homeComponents/MusicPlayerSection/MusicPlayerComponent.tsx
+++ src/app/components/homeComponents/MusicPlayerSection/MusicPlayerComponent.tsx
@@ -1,4 +1,5 @@
 import TitleText from "../../utilityComponents/TitleText";
+import Player from "./Player";
 
 const MusicPlayer = () => {
   return (
@@ -6,6 +7,7 @@ const MusicPlayer = () => {
       <div className="w-fit mx-auto">
         <TitleText text="Night Club Track"></TitleText>
       </div>
+      <Player />
     </div>
   );
 };
